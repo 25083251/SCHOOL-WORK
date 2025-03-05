1aa54c2f4cc101d27faaf8d71429978a079111de
25083251 <25083251@mvula.univen.ac.za>
2025-03-05 19:55:05 +0200

    first commit

---
+++ Dollars.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Dollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("enter the number of dollars: ");
+        int dollars = scanner.nextInt();
+        
+        int Twenties = dollars / 20;
+        dollars %= 20;
+        
+        int Tens = dollars / 10;
+        dollars %= 10;
+        
+        int Fives = dollars / 5;
+        dollars %= 5;
+        
+        int Ones = dollars;
+        
+        System.out.println("Currency denominations:");
+        System.out.println("20s: " + Twenties);
+        System.out.println("10s: " + Tens);
+        System.out.println("5s: " + Fives);
+        System.out.println("1s: " + Ones);
+        
+        scanner.close();
+    }
+}
