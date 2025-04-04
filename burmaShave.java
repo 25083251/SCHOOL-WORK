2ab6f528c062c754595d56d8ac41e84b786a76de
25083251 <25083251@mvula.univen.ac.za>
2025-04-04 12:45:22 +0200

    Update burmaShave.java

--- burmaShave.java
+++ burmaShave.java
@@ -1,14 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class burmaShave {
+
     public static void main(String[] args) {
         String rhyme01 = "THE BEARDED DEVIL ";
         String rhyme02 = "IS FORCED TO DWELL ";
         String rhyme03 = "IN THE ONLY PLACE ";
         String rhyme04 = "WHERE THEY DON’T SELL ";
+        String rhymeBS = "BURMA SHAVING";
 
-        System.out.println(rhyme01 +  "Burma Shave");
-        System.out.println(rhyme02 +  "Burma Shave");
-        System.out.println(rhyme03 +  "Burma Shave");
-        System.out.println(rhyme04 +  "Burma Shave");
+        JOptionPane.showMessageDialog(null,rhyme01 );
+        JOptionPane.showMessageDialog(null,rhyme02 );
+        JOptionPane.showMessageDialog(null,rhyme03 );
+        JOptionPane.showMessageDialog(null,rhyme04 );
+        JOptionPane.showMessageDialog(null,rhymeBS );
+        
     }
     
 }
