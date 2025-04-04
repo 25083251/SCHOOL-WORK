e94001f0d647079c549dfc1d1281277c08a3c584
25083251 <25083251@mvula.univen.ac.za>
2025-04-04 11:32:18 +0200

    Create burmaShave.java

---
+++ burmaShave.java
@@ -0,0 +1,15 @@
+public class burmaShave {
+    public static void main(String[] args) {
+        String rhyme01 = "THE BEARDED DEVIL ";
+        String rhyme02 = "IS FORCED TO DWELL ";
+        String rhyme03 = "IN THE ONLY PLACE ";
+        String rhyme04 = "WHERE THEY DON’T SELL ";
+
+        System.out.println(rhyme01 +  "Burma Shave");
+        System.out.println(rhyme02 +  "Burma Shave");
+        System.out.println(rhyme03 +  "Burma Shave");
+        System.out.println(rhyme04 +  "Burma Shave");
+    }
+    
+}
+ 
